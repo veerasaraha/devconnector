b4a67f1373d674e16ae20addcf9f64362a91910a
veera_1505 <veerasaraha@gmail.com>
2021-07-02 21:28:24 +0530

    config and DB connection

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const connectDB = require('./config/db')
 
 const app = express()
+
+//DB Connection
+connectDB()
 
 app.get('/', (req, res) => {
   res.send(200).json({ message: 'Server Running' })
