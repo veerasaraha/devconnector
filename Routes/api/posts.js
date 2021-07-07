119c6f1a612130aefee2820d2adc5345bada07b7
veera_1505 <veerasaraha@gmail.com>
2021-07-07 20:45:53 +0530

    added like and unlike routes

--- Routes/api/posts.js
+++ Routes/api/posts.js
@@ -127,4 +127,69 @@ router.delete('/:id', auth, async (req, res) => {
   }
 })
 
+// @route  PUT api/posts/like/:id
+// @desc   Like a post
+// @access Private
+router.put('/like/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+
+    // check if the post has already been liked
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.status(400).json({
+        message: 'Post already liked',
+      })
+    }
+
+    post.likes.unshift({ user: req.user.id })
+
+    await post.save()
+
+    res.status(200).json(post.likes)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).json({
+      message: 'Server Error',
+    })
+  }
+})
+
+// @route  PUT api/posts/like/:id
+// @desc   Like a post
+// @access Private
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+
+    // check if the post has already been liked
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({
+        message: 'Post has not yet been liked',
+      })
+    }
+
+    // Get remove index
+    const removeIndex = post.likes
+      .map((like) => like.user.toString())
+      .indexOf(req.user.id)
+
+    post.likes.splice(removeIndex, 1)
+
+    await post.save()
+
+    res.status(200).json(post.likes)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).json({
+      message: 'Server Error',
+    })
+  }
+})
+
 module.exports = router
