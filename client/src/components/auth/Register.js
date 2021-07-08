f46b54398d6b2e320ce861fd9e349b4f72e4d81b
veera_1505 <veerasaraha@gmail.com>
2021-07-08 21:56:19 +0530

    added register component with state

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,87 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 
 const Register = () => {
-  return <div>Register</div>
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  })
+
+  const { name, email, password, confirmPassword } = formData
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setFormData({ ...formData, [name]: value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (password !== confirmPassword) {
+      console.log('Passwords do not match')
+    } else {
+      console.log(formData)
+    }
+  }
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Sign Up</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Create Your Account
+      </p>
+      <form className='form' onSubmit={(e) => handleSubmit(e)}>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Name'
+            name='name'
+            required
+            onChange={(e) => handleChange(e)}
+            valeu={name}
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            required
+            onChange={(e) => handleChange(e)}
+            value={email}
+          />
+          <small className='form-text'>
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            minLength='6'
+            onChange={(e) => handleChange(e)}
+            value={password}
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Confirm Password'
+            name='confirmPassword'
+            minLength='6'
+            onChange={(e) => handleChange(e)}
+            value={confirmPassword}
+          />
+        </div>
+        <input type='submit' className='btn btn-primary' value='Register' />
+      </form>
+      <p className='my-1'>
+        Already have an account? <a href='login.html'>Sign In</a>
+      </p>
+    </Fragment>
+  )
 }
 
 export default Register
