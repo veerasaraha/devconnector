1cacaabdd0b5c48bd3b1eeec7373a049510fa1a2
veera_1505 <veerasaraha@gmail.com>
2021-07-08 20:17:56 +0530

    react setup

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <Fragment>
+      <h1>Hello world</h1>
+    </Fragment>
+  )
+}
+
+export default App
