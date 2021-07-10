c10165ed1543d37878a9f28abca1afaad2ce80a7
veera_1505 <veerasaraha@gmail.com>
2021-07-10 21:40:04 +0530

    added create profile component

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import { loadUser } from './actions/auth'
 import setAuthToken from './utils/setAuthToken'
 import Dashboard from './components/dashboard/Dashboard'
 import PrivateRoute from './components/routing/PrivateRoute'
+import CreateProfile from './components/profile-forms/CreateProfile'
 
 if (localStorage.token) {
   setAuthToken(localStorage.token)
@@ -24,7 +25,7 @@ const App = () => {
 
   return (
     <Provider store={globalStore}>
-      <Router>
+      <Router basename='devconector'>
         <Fragment>
           <Navbar />
           <Route exact path='/' component={Landing} />
@@ -34,6 +35,11 @@ const App = () => {
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute
+                exact
+                path='/create-profile'
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </Fragment>
