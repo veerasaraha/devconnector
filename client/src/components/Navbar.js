f9407184d99879cf19a342c88036596a728900ab
veera_1505 <veerasaraha@gmail.com>
2021-07-11 14:43:45 +0530

    added profiles component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -14,6 +14,11 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
         </Link>
       </li>
       <li>
+        <Link to='/profiles'>
+          <i className='fas fa-users' /> <span>Devlopers</span>
+        </Link>
+      </li>
+      <li>
         <Link to='' onClick={logout}>
           <i className='fas fa-sign-out-alt' />{' '}
           <span className='hide-sm'>Logout</span>
